ece18b99df30061ed565684913dc9cbacbf63722
nxoo <nxoo.dev@gmail.com>
2022-08-26 12:52:05 +0300

    test passed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(array, name) {
+    const matchingDrivers = array.filter(x => x.toLowerCase().includes(name.toLowerCase()));
+    return matchingDrivers;
+};
+
+function fuzzyMatch(array, string) {
+    const length = string.length;
+    const matching = array.filter(y => y.slice(0, length) === string);
+    return matching;
+};
+
+function matchName(array, nameInput) {
+    const matchingDriverNames = array.filter(z => z.name === nameInput);
+    return matchingDriverNames;
+}
